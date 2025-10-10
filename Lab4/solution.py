f846a18136492556af7c6dbc27455372dab2ee7c
[Natasha Eldridge] <neldridge@wdigis.com>
2025-10-10 14:37:07 -0600

    negative check

--- Lab4/solution.py
+++ Lab4/solution.py
@@ -1,9 +1,9 @@
 def radix_base(values_to_sort, base):
     if values_to_sort is None or values_to_sort == [] or base < 2:
         raise ValueError("invalid arguments")
+    
+    for i in values_to_sort:
+        #  for negatives 
+        if i < 0:
+            raise ValueError("invalid list item")
     return 
-
-• If values_to_sort contains values for which mathematical operations such as division
-or modulo are undefned (aka an Exception is raised when you try), or any element of
-values_to_sort are less than zero, radix_base() should raise the
-ValueError("invalid␣list␣element") exception.
