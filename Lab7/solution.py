dfcd2a911a17c4300e360a117a9f6c7847b047c8
[Natasha Eldridge] <neldridge@wdigis.com>
2025-10-31 12:39:17 -0600

    if start not in graph

--- Lab7/solution.py
+++ Lab7/solution.py
@@ -3,4 +3,6 @@ from edgegraph import GraphEL, VertexEL
 def bfs(graph: GraphEL, start: VertexEL) -> list:
     if graph is None or start is None:
         raise ValueError("Invalid graph or vertex")
+    if start not in graph:
+        return []
     pass
