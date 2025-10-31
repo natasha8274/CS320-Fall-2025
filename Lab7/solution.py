925c713174973119d04bb716532d922493a349c5
[Natasha Eldridge] <neldridge@wdigis.com>
2025-10-31 13:14:28 -0600

    get the neighbors of left tree side

--- Lab7/solution.py
+++ Lab7/solution.py
@@ -6,20 +6,24 @@ def bfs(graph: GraphEL, start: VertexEL) -> list:
         raise ValueError("Invalid graph or vertex")
     if start.name not in graph._vertices:
         return []
-    
+
     #  queue to track the traversal
     queue = deque([start])
-    #  the visited vertices
-    visted = {start}
+    #  the visited/explored vertices
+    explored = {start}
     #  list of tuples to be returned at the end
     list_tuples = []
     #  groups vertices that are the same numer of hops from vertex
     current_level_list = [start]
-    
+
     while queue:
         list_tuples.append(tuple(current_level_list))
         current_level_size = len(queue)
         next_level_list = []
-    
-    
+        for _ in range (current_level_size):
+            u = queue.popleft()
+            #  gets all vertices adjacent to u while maintining time complexity 
+            neighbors = graph.adjacent(u)
+
+
     pass
