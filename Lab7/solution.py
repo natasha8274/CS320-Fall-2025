f2865dbefa1033e0122bd6710673a5a0c9f83325
[Natasha Eldridge] <neldridge@wdigis.com>
2025-10-31 12:48:02 -0600

    import deque

--- Lab7/solution.py
+++ Lab7/solution.py
@@ -1,8 +1,11 @@
 from edgegraph import GraphEL, VertexEL
+from collections import deque
 
 def bfs(graph: GraphEL, start: VertexEL) -> list:
     if graph is None or start is None:
         raise ValueError("Invalid graph or vertex")
-    if start not in graph:
+    if start.name not in graph._vertices:
         return []
+    
+    queue = deque([start])
     pass
