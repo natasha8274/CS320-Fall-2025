5124d11058d4d4253c3e0f8dff4e511be0538259
[Natasha Eldridge] <neldridge@wdigis.com>
2025-11-16 11:59:04 -0700

    create the helper function

--- Lab8/solution.py
+++ Lab8/solution.py
@@ -1,19 +1,28 @@
 from edgegraph import GraphEL, VertexEL, EdgeEL
 from collections import Counter
 
-def pld_graph(g: GraphEL) -> list:
+def pld_graph(g: GraphEL):
     if g is None:
         raise ValueError("Bad graph")
     
     #  set of tuples so that they will not be duplicated
-    set_tuples = set()
+    found_palindromes = set()
     
     edge_count = Counter()
     for edge in g.edges():
         edge_count[edge.get_value()] += 1
     
     #  return empty if no tuples
-    if not set_tuples:
+    if not found_palindromes:
         return []
-    
-    return list(set_tuples)
+
+    return list(found_palindromes)
+
+#  send the sequence into the helper function
+def is_palindrome(s):
+    #  check there are at least 3 edeg values
+    if len(s) < 3:
+        return False
+
+    #  Check if the sequence is same both ways
+    return s == s[::-1]
