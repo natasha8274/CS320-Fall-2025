d98c017458a49b42567b958231cf38a23c4f09a4
[Natasha Eldridge] <neldridge@wdigis.com>
2025-11-16 12:18:24 -0700

    adding traversal function

--- Lab8/solution.py
+++ Lab8/solution.py
@@ -8,6 +8,8 @@ def pld_graph(g: GraphEL):
     #  set of tuples so that they will not be duplicated
     found_palindromes = set()
     
+    for edge in g.edged():
+        
     #  return empty if no tuples
     if not found_palindromes:
         return []
@@ -22,3 +24,23 @@ def is_palindrome(s):
 
     #  Check if the sequence is same both ways
     return s == s[::-1]
+
+#  traversal function 
+def _find_paths_from_edge(g, curr_vertex, path_values, used_edges, found_palindromes):
+    #  check curr depth for palindromes
+    if is_palindrome(path_values):
+        found_palindromes.add(tuple(path_values))
+    
+    for edge in g.incident(curr_vertex):
+        if edge not in used_edges:
+            ends = edge.ends()
+            v1 = ends[0]
+            v2 = ends[1]
+            if v1 == curr_vertex:
+                next_vertex = v2
+            else:
+                next_vertex = v1
+            
+                
+    
+            
