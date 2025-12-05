d4e1ebb8893bee6a6a7f61ef97f16ba5d0f8a818
[Natasha Eldridge] <neldridge@wdigis.com>
2025-12-05 08:38:31 -0700

    initaialise

--- Lab9/helper_methods_with_two_example_worlds/solution.py
+++ Lab9/helper_methods_with_two_example_worlds/solution.py
@@ -1,5 +1,14 @@
+import random 
 
-def ga_tsp(initial_population, distances, generations){
+def ga_tsp(initial_population, distances, generations):
     
-    if initial_population is None
-}
+    if initial_population is None or distances is None or generations is None:
+        raise ValueError("Invalid argument")
+        
+    if generations <= 0:
+        raise ValueError("Invalid argiment")
+    
+    population = initial_population
+    population_size = len(population)
+    
+    
