155161c2756d743cdcb74342fa232e1e2d06f9c1
[Natasha Eldridge] <neldridge@wdigis.com>
2025-12-05 09:20:48 -0700

    finsihed both helpers

--- Lab9/helper_methods_with_two_example_worlds/solution.py
+++ Lab9/helper_methods_with_two_example_worlds/solution.py
@@ -4,8 +4,38 @@ from util import cost, best_path
 
 
 def optimize_path_crossover(parent1, parent2):
-    child = [None]
+    size = len(parent1)
+    
+    start = random.randint(0, size - 1)
+    end = random.randint(0, size - 1)
+    
+    if start > end:
+        start, end = end, start
+    
+    #  Initialize the child with None to track the empty spots
+    child = [None] * size
+    
+    for i in range(start, end + 1):
+        child[i]  = parent1[i]
+        
+    curr_position = (end + 1) % size
+    p2_position = (end + 1) % size
+    
+    while None in child:
+        city = parent2[p2_position]
+        #  can only add the city if it isnt in the child 
+        if city not in child:
+            child[curr_position] = citycurr_position = (curr_position + 1) % size
+        p2_position = (p2_position + 1) % size
+    
     return tuple(child)
+def mutate_path(path):
+    path_list = list(path)
+    index1, index2 = random.sample(range(len(path)), 2)
+    path_list[index1], path_list[index2] = path_list[index2], path_list[index1]
+    return tuple(path_list)
+
+
 def ga_tsp(initial_population, distances, generations):
     
     if initial_population is None or distances is None or generations is None:
@@ -23,12 +53,16 @@ def ga_tsp(initial_population, distances, generations):
         parents = sorted_pop[:population_size // 2]
         
         children = []
+        
         while len(children) < population_size:
             #  choose two parents randomly from the elite pool
             p1 = random.choice(parents)
             p2 = random.choice(parents)
             
             child = optimize_path_crossover(p1, p2)
+            
+            if random.random() < 0.1:
+                child = mutate_path(child)
             
         
     
